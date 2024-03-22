036634272b50caeccef038ed4233b0d9f3177c54
Abhi-y2003 <abhisheks2651@gmail.com>
2024-03-23 00:18:03 +0530

    midway

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pagination  from "./components/Pagination";
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+
+      <Blogs/>
+
+      <Pagination/>
+
     </div>
   );
 }
---
+++ src/context/AppContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const AppContext = createContext();   //frist step done. 
+
+function AppContextProvider({children}){
+    
+}
